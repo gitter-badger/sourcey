4ab85e568f550cf5f730ca21ad095c706f7603f0
Daniel Baker <danbaker@me.com>
2015-08-03 18:28:34 +0100

    rudimentary build system for development use that will transpire ES6/JSX to regular vanilla JS. Has it's bugs, and the auto refresh doesn't like it, but it's closer.

--- src/ui/index.js
+++ src/ui/index.js
@@ -1,11 +1,19 @@
 var React = require( "react" );
 
+var ipc = require( "ipc" );
+ipc.on( "dev-refresh", function(message) {
+    if ( message ) {
+        window.location.reload();
+    }
+} );
+
+var remote = require( "remote" );
+console.log( "app version " + remote.require( "app" ).getVersion() );
+
 var start_app = function() {
     var HelloMessage = require( "./components/hello-world" );
     var mountNode = document.querySelector( "#mountNode" );
     React.render( React.createElement( HelloMessage, { name: "World!" } ), mountNode );
 };
 
-module.exports = {
-    start: start_app
-};
+start_app();
