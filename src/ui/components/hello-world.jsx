e5b98dfb0cd7e990ac80f85203599521e6417639
Daniel Baker <danbaker@me.com>
2015-08-04 17:54:46 +0100

    Made the hello world have a stateful binding example

--- src/ui/components/hello-world.jsx
+++ src/ui/components/hello-world.jsx
@@ -3,14 +3,22 @@
 var React = require( "react" );
 
 class HelloWorldComponent extends React.Component {
+    constructor( props ) {
+        super( props );
+        this.state = { word: "World" };
+        this.wordDidChange = this.wordDidChange.bind( this );
+    }
+    wordDidChange( e ) {
+        this.setState( { word: e.target.value } );
+    }
     render() {
         return <div className="jumbotron">
-            <h1>Hello {this.props.name}!</h1>
+            <h1>Hello {this.state.word}!</h1>
             <p>This is a simple example of the Sourcey App</p>
             <form className="form-inline">
                 <div className="form-group">
                     <label>Name:&nbsp;</label>
-                    <input type="text" className="form-control" id="name-input" placeholder={this.props.name} />
+                    <input type="text" className="form-control" id="name-input" placeholder={this.state.word} onKeyUp={this.wordDidChange} />
                 </div>
             </form>
         </div>
