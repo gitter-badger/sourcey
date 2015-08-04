d4bde28e8fa2c58cf7b216a50f1ede1c273629ab
Daniel Baker <danbaker@me.com>
2015-08-04 17:21:54 +0100

    simple jumbotron hello world example

--- src/ui/components/hello-world.jsx
+++ src/ui/components/hello-world.jsx
@@ -4,7 +4,16 @@ var React = require( "react" );
 
 class HelloWorldComponent extends React.Component {
     render() {
-        return <div>Hello {this.props.name}!</div>;
+        return <div className="jumbotron">
+            <h1>Hello {this.props.name}!</h1>
+            <p>This is a simple example of the Sourcey App</p>
+            <form className="form-inline">
+                <div className="form-group">
+                    <label>Name:&nbsp;</label>
+                    <input type="text" className="form-control" id="name-input" placeholder={this.props.name} />
+                </div>
+            </form>
+        </div>
     }
 };
 
