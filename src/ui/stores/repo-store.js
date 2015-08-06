8efe863e67ffe459ddafd720ca59d20725573134
Daniel Baker <danbaker@me.com>
2015-08-06 14:05:04 +0100

    added basic nodegit opening a repo - not tested as having issues currently getting nodegit to build for electron

--- src/ui/stores/repo-store.js
+++ src/ui/stores/repo-store.js
@@ -1,11 +1,21 @@
 var Reflux = require( "reflux" );
 var RepoActions = require( "../actions/repo-actions" );
+var NodeGit = require( "nodegit" );
+var path = require( "path" );
 
 var RepoStore = Reflux.createStore( {
     listenables: [ RepoActions ],
     openRepo: function( path ) {
-        this.repo = path;
-        this.trigger( this.repo );
+        var self = this;
+        self.repo = require( "path" ).resolve( path );
+        NodeGit.Repository.open( self.repo ).then( function( repo ) {
+            repo.getReferenceNames( 3 ).then( function( references ) {
+                self.trigger( references.toString() );
+            } );
+        } )
+        .catch( function( error ) {
+            console.log( error );
+        } );
     }
 } );
 
